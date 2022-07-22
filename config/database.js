fbae805ba24aa3310a95d81fa178b59021311e15
fathy17 <fathyfahrzy17@gmail.com>
2022-07-22 18:07:17 +0800

    migrate v3

---
+++ config/database.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "vadhel-portfolio"),
+        username: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "root"),
+        schema: env("DATABASE_SCHEMA", "vadhel-portfolio"), // Not Required
+        ssl: false,
+      },
+      options: {},
+    },
+  },
+});
